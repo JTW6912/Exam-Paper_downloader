5fc37ea947a5fa7dc0366ba1ffc999f1e2ffa760
JTW6912 <143696343+JTW6912@users.noreply.github.com>
2023-09-17 16:36:09 +0100

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-
-window = tk.Tk()
-ListContent = tk.StringVar()
-ListContent.set(['chinese','utuber'])
-Lb = tk.Listbox(window,listvariable=ListContent)
-Lb.pack()
-
-window.mainloop()
